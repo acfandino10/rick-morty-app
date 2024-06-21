b5d70c381eaf3cb8171b06337328259f54f9c526
Ana Fandino <ac.fandino10@uniandes.edu.co>
2024-06-21 03:50:35 -0500

    [RM-1] 💅STYLED(_components/CharactersList): styled characters list

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -3,16 +3,17 @@
 import { useEffect } from "react";
 import CharactersList from "components/CharactersList";
 import { withApollo } from "lib/withApollo";
+import localFont from "next/font/local";
+
+const myFont = localFont({ src: "../_fonts/GreyCliff.otf" });
 
 function Home() {
-
-
   useEffect(() => {
     document.title = "Rick and Morty";
   }, []);
 
   return (
-    <main className="flex max-h-screen flex-col justify-flex-start h-lvh">
+    <main className={["flex max-h-screen flex-col justify-flex-start h-lvh", myFont.className].join(" ")}>
       <CharactersList title="Rick and Morty list" />
     </main>
   );
--- src/app/_components/CharactersList.tsx
+++ src/app/_components/CharactersList.tsx
@@ -10,6 +10,7 @@ import { Character } from "types/character";
 import { FavoriteBorderOutlined } from "@mui/icons-material";
 import { useQuery } from "@apollo/client";
 import { GET_CHARACTERS } from "../_graphql/queries/GetCharacters";
+import { Box } from "@mui/material";
 
 interface CharactersListProps {
   title: string;
@@ -26,50 +27,89 @@ export default function CharactersList({ title }: CharactersListProps) {
     }
   }, [data]);
   return (
-    
-      <List
+    <List
+      sx={{
+        width: "375px",
+        paddingTop: "42px",
+        fontFamily: "GreyCliff",
+        paddingRight: "13px",
+        paddingLeft: "19px",
+        maxWidth: "sm",
+        maxHeight: "100vh",
+        background: "linear-gradient(to right, white, #FBFBFB)",
+        overflow: "auto",
+      }}
+    >
+      <Typography
+        variant="h6"
+        gutterBottom
+        pb={"25px"}
         sx={{
-          width: "375px",
-          paddingTop: "42px",
-          paddingRight: "13px",
-          paddingLeft: "19px",
-          maxWidth: "sm",
-          maxHeight: "100vh",
-          bgcolor: "background.paper",
-          overflow: "auto",
+          color: "#1F2937",
+          fontSize: "24px",
+          fontWeight: 400,
+          marginLeft: 2,
+          fontFamily: "GreyCliff",
         }}
       >
-        <Typography variant="h6" gutterBottom pb={"25px"} sx={{ color:"#1F2937", fontSize: "24px", fontWeight: 400}}>
-          {title}
-        </Typography>
-        <Typography variant="body1" pb={"25px"} sx={{ color: "#6B7280", fontSize: "12px", fontWeight: 600 }} gutterBottom>
-          CHARACTERS ({characters.length})
-        </Typography>
-        <Suspense fallback={<div>Loading character details...</div>}>
+        {title}
+      </Typography>
+      <Box
+        sx={{
+          pb: "22px",
+          color: "#6B7280",
+          fontSize: "12px",
+          fontWeight: 600,
+          marginLeft: 2, 
+          fontFamily: "GreyCliff",
+        }}
+      >
+        CHARACTERS
+        <span style={{ fontFamily: "Roboto" }}>{" ("}</span>
+        {characters.length}
+        <span style={{ fontFamily: "Roboto" }}>{")"}</span>
+      </Box>
+      <Suspense fallback={<div>Loading characters list...</div>}>
         {characters?.map((character) => (
           <Fragment key={character.id}>
-          <Divider component="li" sx={{marginLeft: 2, marginRight: 2}} />
+            <Divider component="li" sx={{ marginLeft: 2, marginRight: 2 }} />
             <ListItem alignItems="center">
               <ListItemAvatar>
                 <Avatar alt={character.name} src={character.image} />
               </ListItemAvatar>
               <ListItemText
                 primary={
-                  <Typography variant="body1" sx={{ color: "#1F2937", fontSize: "16px", fontWeight: 600 }}>
+                  <Typography
+                    variant="body1"
+                    sx={{
+                      color: "#1F2937",
+                      fontSize: "16px",
+                      fontWeight: 600,
+                      fontFamily: "GreyCliff",
+                    }}
+                  >
                     {character.name}
                   </Typography>
                 }
                 secondary={
-                <Typography variant="body2" sx={{ color: "#6B7280", fontSize: "16px", fontWeight: 400 }}>
-                  {character.species}
-                </Typography>
+                  <Typography
+                    variant="body2"
+                    sx={{
+                      color: "#6B7280",
+                      fontSize: "16px",
+                      fontWeight: 400,
+                      fontFamily: "GreyCliff",
+                    }}
+                  >
+                    {character.species}
+                  </Typography>
                 }
               />
-              <FavoriteBorderOutlined htmlColor={'#D1D5DB'}/>
+              <FavoriteBorderOutlined htmlColor={"#D1D5DB"} />
             </ListItem>
           </Fragment>
         ))}
-        </Suspense>
-      </List>
+      </Suspense>
+    </List>
   );
 }
