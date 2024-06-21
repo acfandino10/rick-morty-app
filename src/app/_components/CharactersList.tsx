b210672dbe39f226d1b2b0cbab420335ccf3b667
Ana Fandino <ac.fandino10@uniandes.edu.co>
2024-06-21 07:33:54 -0500

    [RM-3] 🚧WIP(_components/CharactersList): add filter to list

--- src/app/_components/CharactersList.tsx
+++ src/app/_components/CharactersList.tsx
@@ -8,23 +8,103 @@ import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
 import { Character } from "types/character";
-import { FavoriteBorderOutlined, FavoriteBorderRounded, FavoriteRounded } from "@mui/icons-material";
+import {
+  FavoriteBorderOutlined,
+  FavoriteBorderRounded,
+  FavoriteRounded,
+} from "@mui/icons-material";
 import { useQuery } from "@apollo/client";
 import { GET_CHARACTERS } from "../_graphql/queries/GetCharacters";
-import { Box } from "@mui/material";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Card,
+  IconButton,
+  InputBase,
+  Popover,
+} from "@mui/material";
+import styled from "styled-components";
+import SearchSvg from "../_assets/search.svg";
+import FilterSvg from "../_assets/filter.svg";
 
 interface CharactersListProps {
   title: string;
   onSelected: (character: Character) => void;
 }
 
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  "&:hover": {
+    // backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  borderRadius: "8px",
+  padding: "9px 0px 9px 20px",
+  marginBottom: "40px",
+  gap: "8px",
+  // marginLeft: 10,
+  backgroundColor: "#F3F4F6",
+  height: "52px",
+  width: "343px",
+  alignContent: "center",
+}));
+
+const StyledButton = styled(Button)(({ theme }) => ({
+  margin: 10,
+  border: "1px solid #6B7280",
+  borderRadius: "5px",
+  width: "100px",
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  height: "100%",
+  position: "absolute",
+  left: 10,
+  top: 0,
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const FilterIconWrapper = styled("div")(({ theme }) => ({
+  // padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  right: 30,
+  top: 0,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    // padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: 45,
+    // transition: theme.transitions.create("width"),
+    width: "100%",
+    // [theme.breakpoints.up("sm")]: {
+    //   width: "12ch",
+    //   "&:focus": {
+    //     width: "20ch",
+    //   },
+    // },
+  },
+}));
+
 export default function CharactersList({
   title,
   onSelected,
 }: CharactersListProps) {
   const [characters, setCharacters] = useState<Character[]>();
+  const [openFilter, setOpenFilter] = useState(false);
   const [starredIds, setStarredIds] = useState<number[]>([]); // [1, 2, 3
   const { loading, error, data } = useQuery(GET_CHARACTERS);
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
+    null
+  );
 
   const selectCharacter = (character: Character) => {
     // TODO. Also change selection color
@@ -40,7 +120,7 @@ export default function CharactersList({
       const newStarredIds = starredIds.filter((starredId) => starredId !== id);
       setStarredIds(newStarredIds);
     }
-  }
+  };
 
   useEffect(() => {
     document.title = "Rick and Morty";
@@ -79,6 +159,77 @@ export default function CharactersList({
           {title}
         </Typography>
         {loading && <p>Loading characters list...</p>}
+        {!loading && (
+          <>
+            <Search>
+              <SearchIconWrapper>
+                <SearchSvg />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search or filter results"
+                inputProps={{ "aria-label": "search" }}
+                color="success"
+              />
+              <FilterIconWrapper>
+                <IconButton
+                  aria-label="filter"
+                  sx={{ zIndex: 10 }}
+                  onClick={() => setOpenFilter(!openFilter)}
+                >
+                  <FilterSvg />
+                </IconButton>
+                <Popover
+                  open={openFilter}
+                  anchorEl={anchorEl}
+                  onClose={() => setOpenFilter(false)}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "left",
+                  }}
+                >
+                  <Card>
+                    <Box sx={{ p: 2 }}>
+                      <Typography
+                          variant="body2"
+                          sx={{
+                            color: "#6B7280",
+                            fontSize: "16px",
+                          }}
+                        >
+                          Character
+                        </Typography>
+                      <ButtonGroup
+                        variant="outlined"
+                        aria-label="Basic button group"
+                      >
+                        <StyledButton>All</StyledButton>
+                        <StyledButton>Starred</StyledButton>
+                        <StyledButton>Others</StyledButton>
+                      </ButtonGroup><Typography
+                          variant="body2"
+                          sx={{
+                            color: "#6B7280",
+                            fontSize: "16px",
+                          }}
+                        >
+                          Specie
+                        </Typography>
+                      <ButtonGroup
+                        variant="outlined"
+                        aria-label="Basic button group"
+                      >
+                        <StyledButton>All</StyledButton>
+                        <StyledButton>Human</StyledButton>
+                        <StyledButton>Alien</StyledButton>
+                      </ButtonGroup>
+                      <Button fullWidth>Filter</Button>
+                    </Box>
+                  </Card>
+                </Popover>
+              </FilterIconWrapper>
+            </Search>
+          </>
+        )}
         {!loading && starredIds.length > 0 && (
           <>
             <Box
@@ -96,50 +247,55 @@ export default function CharactersList({
               {characters?.length}
               <span style={{ fontFamily: "Roboto" }}>{")"}</span>
             </Box>
-            {characters?.filter(el => starredIds.includes(el.id))?.map((character) => (
-              <Fragment key={character.id}>
-                <Divider
-                  component="li"
-                  sx={{ marginLeft: 2, marginRight: 2 }}
-                />
-                <ListItem
-                  alignItems="center"
-                  onClick={() => selectCharacter(character)}
-                >
-                  <ListItemAvatar>
-                    <Avatar alt={character.name} src={character.image} />
-                  </ListItemAvatar>
-                  <ListItemText
-                    primary={
-                      <Typography
-                        variant="body1"
-                        sx={{
-                          color: "#1F2937",
-                          fontSize: "16px",
-                          fontWeight: 600,
-                          fontFamily: "GreyCliff",
-                        }}
-                      >
-                        {character.name}
-                      </Typography>
-                    }
-                    secondary={
-                      <Typography
-                        variant="body2"
-                        sx={{
-                          color: "#6B7280",
-                          fontSize: "16px",
-                        }}
-                      >
-                        {character.species}
-                      </Typography>
-                    }
+            {characters
+              ?.filter((el) => starredIds.includes(el.id))
+              ?.map((character) => (
+                <Fragment key={character.id}>
+                  <Divider
+                    component="li"
+                    sx={{ marginLeft: 2, marginRight: 2 }}
                   />
-                  <FavoriteRounded sx={{ zIndex: 10 }} color="primary" onClick={()=>selectFavorite(character.id)} />
-
-                </ListItem>
-              </Fragment>
-            ))}
+                  <ListItem
+                    alignItems="center"
+                    onClick={() => selectCharacter(character)}
+                  >
+                    <ListItemAvatar>
+                      <Avatar alt={character.name} src={character.image} />
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary={
+                        <Typography
+                          variant="body1"
+                          sx={{
+                            color: "#1F2937",
+                            fontSize: "16px",
+                            fontWeight: 600,
+                            fontFamily: "GreyCliff",
+                          }}
+                        >
+                          {character.name}
+                        </Typography>
+                      }
+                      secondary={
+                        <Typography
+                          variant="body2"
+                          sx={{
+                            color: "#6B7280",
+                            fontSize: "16px",
+                          }}
+                        >
+                          {character.species}
+                        </Typography>
+                      }
+                    />
+                    <FavoriteRounded
+                      sx={{ zIndex: 10 }}
+                      color="primary"
+                      onClick={() => selectFavorite(character.id)}
+                    />
+                  </ListItem>
+                </Fragment>
+              ))}
           </>
         )}
         {!loading && (
@@ -159,51 +315,64 @@ export default function CharactersList({
               {characters?.length}
               <span style={{ fontFamily: "Roboto" }}>{")"}</span>
             </Box>
-            {characters?.filter(el => !starredIds.includes(el.id))?.map((character) => (
-              <Fragment key={character.id}>
-                <Divider
-                  component="li"
-                  sx={{ marginLeft: 2, marginRight: 2 }}
-                />
-                <ListItem
-                  alignItems="center"
-                  onClick={() => selectCharacter(character)}
-                >
-                  <ListItemAvatar>
-                    <Avatar alt={character.name} src={character.image} />
-                  </ListItemAvatar>
-                  <ListItemText
-                    primary={
-                      <Typography
-                        variant="body1"
-                        sx={{
-                          color: "#1F2937",
-                          fontSize: "16px",
-                          fontWeight: 600,
-                          fontFamily: "GreyCliff",
-                        }}
-                      >
-                        {character.name}
-                      </Typography>
-                    }
-                    secondary={
-                      <Typography
-                        variant="body2"
-                        sx={{
-                          color: "#6B7280",
-                          fontSize: "16px",
-                        }}
-                      >
-                        {character.species}
-                      </Typography>
-                    }
+            {characters
+              ?.filter((el) => !starredIds.includes(el.id))
+              ?.map((character) => (
+                <Fragment key={character.id}>
+                  <Divider
+                    component="li"
+                    sx={{ marginLeft: 2, marginRight: 2 }}
                   />
-                  {!starredIds.includes(character.id) &&<FavoriteBorderOutlined sx={{ zIndex: 10 }} htmlColor="#D1D5DB" onClick={()=>selectFavorite(character.id)} />}
-                  {starredIds.includes(character.id) && <FavoriteBorderRounded sx={{ zIndex: 10 }} color="primary" onClick={()=>selectFavorite(character.id)} />}
-
-                </ListItem>
-              </Fragment>
-            ))}
+                  <ListItem
+                    alignItems="center"
+                    onClick={() => selectCharacter(character)}
+                  >
+                    <ListItemAvatar>
+                      <Avatar alt={character.name} src={character.image} />
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary={
+                        <Typography
+                          variant="body1"
+                          sx={{
+                            color: "#1F2937",
+                            fontSize: "16px",
+                            fontWeight: 600,
+                            fontFamily: "GreyCliff",
+                          }}
+                        >
+                          {character.name}
+                        </Typography>
+                      }
+                      secondary={
+                        <Typography
+                          variant="body2"
+                          sx={{
+                            color: "#6B7280",
+                            fontSize: "16px",
+                          }}
+                        >
+                          {character.species}
+                        </Typography>
+                      }
+                    />
+                    {!starredIds.includes(character.id) && (
+                      <FavoriteBorderOutlined
+                        sx={{ zIndex: 10 }}
+                        htmlColor="#D1D5DB"
+                        onClick={() => selectFavorite(character.id)}
+                      />
+                    )}
+                    {starredIds.includes(character.id) && (
+                      <FavoriteBorderRounded
+                        sx={{ zIndex: 10 }}
+                        color="primary"
+                        onClick={() => selectFavorite(character.id)}
+                      />
+                    )}
+                  </ListItem>
+                </Fragment>
+              ))}
           </>
         )}
       </List>
